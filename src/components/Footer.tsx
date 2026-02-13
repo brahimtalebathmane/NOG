ea880e4b245a56ddac658f27c2f122d95471a018
brahimtalebathmane <brahimtalebathmane@gmail.com>
2026-02-13 12:15:22 +0000

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -84,7 +84,7 @@ export const Footer = ({ onNavigate }: FooterProps) => {
             </div>
             <div className={`${isRTL ? 'text-right' : 'text-left'}`}>
               <p><span className="font-semibold">{language === 'ar' ? 'بنكيلي:' : 'Bank:'}</span> 36606886 +222</p>
-              <p><span className="font-semibold">{language === 'ar' ? 'مصرفي:' : 'Bank ID:'}</span> +222 44444555</p>
+              <p><span className="font-semibold">{language === 'ar' ? 'مصرفي:' : 'Bank ID:'}</span> 44444555 +222</p>
               <p><span className="font-semibold">{language === 'ar' ? 'السداد:' : 'Payment:'}</span> 03650</p>
             </div>
           </div>
